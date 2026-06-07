2586c6894d2f8e812399aa5d5a393559bf20809c
xasmedy <xasmedy@pm.me>
2026-06-07 18:32:30 +0200

    Added default Affine2Ops

--- src/main/java/org/lidiuma/math/api/matrix/Affine2Ops.java
+++ src/main/java/org/lidiuma/math/api/matrix/Affine2Ops.java
@@ -28,11 +28,154 @@ public interface Affine2Ops<
     A of(N m00, N m01, N m02,
          N m10, N m11, N m12);
 
+    @Override
+    default A identity() {
+        final var witness = scalarOps();
+        final var one = witness.one();
+        final var zero = witness.zero();
+        return of(
+                one, zero, zero,
+                zero, one, zero
+        );
+    }
+
     /// @return the angle calculated from the rotation component of this matrix.
     AN rotation(A affine);
 
     /// Transposes the 2x2 sub-matrix (linear part) of this affine matrix, ignoring the translation part.
     /// @return the transposed affine matrix.
     @Override
-    A transpose(A affine);
+    default A transpose(A affine) {
+        return of(
+                affine.m00(), affine.m10(), affine.m02(),
+                affine.m01(), affine.m11(), affine.m12()
+        );
+    }
+
+    @Override
+    default N determinant(A matrix) {
+        final var witness = scalarOps();
+        final N m0011 = witness.multiply(matrix.m00(), matrix.m11());
+        final N m0110 = witness.multiply(matrix.m01(), matrix.m10());
+        return witness.subtract(m0011, m0110);
+    }
+
+    @Override
+    default A inverse(A matrix) throws ArithmeticException {
+
+        final var ops = scalarOps();
+        final N det = determinant(matrix);
+        if (det.equals(ops.zero())) throw new ArithmeticException("The matrix cannot be inverted since singular.");
+
+        final N invDet = ops.divide(ops.one(), det);
+
+        final N m02 = ops.subtract(ops.multiply(matrix.m01(), matrix.m12()), ops.multiply(matrix.m11(), matrix.m02()));
+        final N m12 = ops.subtract(ops.multiply(matrix.m10(), matrix.m02()), ops.multiply(matrix.m00(), matrix.m12()));
+
+        final N n00 = ops.multiply(invDet, matrix.m11());
+        final N n01 = ops.multiply(invDet, ops.negated(matrix.m01()));
+        final N n02 = ops.multiply(invDet, m02);
+        final N n10 = ops.multiply(invDet, ops.negated(matrix.m10()));
+        final N n11 = ops.multiply(invDet, matrix.m00());
+        final N n12 = ops.multiply(invDet, m12);
+        return of(
+                n00, n01, n02,
+                n10, n11, n12
+        );
+    }
+
+    @Override
+    default boolean isSingular(A matrix) {
+        return determinant(matrix).equals(scalarOps().zero());
+    }
+
+    @Override
+    default A multiply(A matrix, N scalar) {
+        final var ops = scalarOps();
+        final N m00 = ops.multiply(matrix.m00(), scalar);
+        final N m10 = ops.multiply(matrix.m10(), scalar);
+        final N m01 = ops.multiply(matrix.m01(), scalar);
+        final N m11 = ops.multiply(matrix.m11(), scalar);
+        final N m02 = ops.multiply(matrix.m02(), scalar);
+        final N m12 = ops.multiply(matrix.m12(), scalar);
+        return of(
+                m00, m01, m02,
+                m10, m11, m12
+        );
+    }
+
+    @Override
+    default A add(A op1, A op2) {
+        final var ops = scalarOps();
+        final N m00 = ops.add(op1.m00(), op2.m00());
+        final N m10 = ops.add(op1.m10(), op2.m10());
+        final N m01 = ops.add(op1.m01(), op2.m01());
+        final N m11 = ops.add(op1.m11(), op2.m11());
+        final N m02 = ops.add(op1.m02(), op2.m02());
+        final N m12 = ops.add(op1.m12(), op2.m12());
+        return of(
+                m00, m01, m02,
+                m10, m11, m12
+        );
+    }
+
+    @Override
+    default A subtract(A op1, A op2) {
+        final var ops = scalarOps();
+        final N m00 = ops.subtract(op1.m00(), op2.m00());
+        final N m10 = ops.subtract(op1.m10(), op2.m10());
+        final N m01 = ops.subtract(op1.m01(), op2.m01());
+        final N m11 = ops.subtract(op1.m11(), op2.m11());
+        final N m02 = ops.subtract(op1.m02(), op2.m02());
+        final N m12 = ops.subtract(op1.m12(), op2.m12());
+        return of(
+                m00, m01, m02,
+                m10, m11, m12
+        );
+    }
+
+    @Override
+    default A multiply(A op1, A op2) {
+        final var ops = scalarOps();
+        final N m00 = ops.add(ops.multiply(op1.m00(), op2.m00()), ops.multiply(op1.m01(), op2.m10()));
+        final N m01 = ops.add(ops.multiply(op1.m00(), op2.m01()), ops.multiply(op1.m01(), op2.m11()));
+        final N m02 = ops.add(ops.multiply(op1.m00(), op2.m02()), ops.multiply(op1.m01(), op2.m12()));
+        final N m10 = ops.add(ops.multiply(op1.m10(), op2.m00()), ops.multiply(op1.m11(), op2.m10()));
+        final N m11 = ops.add(ops.multiply(op1.m10(), op2.m01()), ops.multiply(op1.m11(), op2.m11()));
+        final N m12 = ops.add(ops.multiply(op1.m10(), op2.m02()), ops.multiply(op1.m11(), op2.m12()));
+        return of(
+                m00, m01, ops.add(m02, op1.m02()),
+                m10, m11, ops.add(m12, op1.m12())
+        );
+    }
+
+    @Override
+    default A remainder(A op1, A op2) {
+        final var ops = scalarOps();
+        final N m00 = ops.remainder(op1.m00(), op2.m00());
+        final N m10 = ops.remainder(op1.m10(), op2.m10());
+        final N m01 = ops.remainder(op1.m01(), op2.m01());
+        final N m11 = ops.remainder(op1.m11(), op2.m11());
+        final N m02 = ops.remainder(op1.m02(), op2.m02());
+        final N m12 = ops.remainder(op1.m12(), op2.m12());
+        return of(
+                m00, m01, m02,
+                m10, m11, m12
+        );
+    }
+
+    @Override
+    default A negated(A operand) {
+        final var ops = scalarOps();
+        final N m00 = ops.negated(operand.m00());
+        final N m10 = ops.negated(operand.m10());
+        final N m01 = ops.negated(operand.m01());
+        final N m11 = ops.negated(operand.m11());
+        final N m02 = ops.negated(operand.m02());
+        final N m12 = ops.negated(operand.m12());
+        return of(
+                m00, m01, m02,
+                m10, m11, m12
+        );
+    }
 }
